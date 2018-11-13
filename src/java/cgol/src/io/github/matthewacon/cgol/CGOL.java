df0946bd8ffc0c7c14f403a44d552613a3a01298
matthewjbarichello@gmail.com <matthewjbarichello@gmail.com>
2018-11-12 21:51:41 -0500

    Multiple ruleset implementation and build automation

--- src/java/cgol/src/io/github/matthewacon/cgol/CGOL.java
+++ src/java/cgol/src/io/github/matthewacon/cgol/CGOL.java
@@ -1,10 +1,17 @@
 package io.github.matthewacon.cgol;
 
 import java.util.AbstractMap.SimpleEntry;
-import java.util.Arrays;
 import java.util.Random;
 
+//TODO functional interface for underlying structure
+//Functional interface for neighbour checking
+//Functional interface for rule set
+//Default implementation of iteration
 public final class CGOL {
+ interface StepFunction {
+  void step(final CGOL board);
+ }
+
  private Boolean[][] board;
 
  public CGOL(final int width, final int height) {
@@ -16,8 +23,9 @@ public final class CGOL {
   final Random random = new Random();
   for (int i = 0; i < board.length; i++) {
    for (int j = 0; j < board[i].length; j++) {
-    board[i][j] = random.nextBoolean();
-//    board[i][j] = (random.nextInt(100000) + 1) < 50000;
+//    board[i][j] = random.nextBoolean();
+    //Add (1/4) bias (on/off)
+    board[i][j] = (random.nextInt(100000) + 1) < 25000;
    }
   }
  }
@@ -42,41 +50,72 @@ public final class CGOL {
   } else if (j > board[0].length - 1) {
    while ((j -= board[0].length) >= board[0].length);
   }
-//  System.out.println(i + " :: " + j);
   return new SimpleEntry<>(i, j);
  }
 
- public Boolean[][] step() {
-  final Boolean[][] future = Arrays.copyOf(board, board.length);
-  for (int i = 0; i < board.length; i++) {
-   for (int j = 0; j < board[i].length; j++) {
-    //Count neighbours
-    int neighbours = 0;
-    for (int l = -1; l <= 1; l++) {
-     for (int m = -1; m <= 1; m++) {
-//      if (!(l == i && m == j)) {
-       final SimpleEntry<Integer, Integer> indices = wrap(i + l, j + m);
-       if (board[indices.getKey()][indices.getValue()]) {
-        neighbours++;
-       }
+ public Boolean[][] step(final StepFunction... sfs) {
+////  final Boolean[][] future = Arrays.copyOf(board, board.length);
+////  final Boolean[][] future = new Boolean[board.length][board[0].length];
+//  for (int i = 0; i < board.length; i++) {
+//   for (int j = 0; j < board[i].length; j++) {
+//    //Count neighbours
+//    int neighbours = 0;
+//    for (int l = -1; l < 2; l++) {
+//     for (int m = -1; m < 2; m++) {
+////      if (!(l == i && m == j)) {
+////      if (l != 0 && m != 0) {
+//       final SimpleEntry<Integer, Integer> indices = wrap(i + l, j + m);
+//       if (board[indices.getKey()][indices.getValue()]) {
+//        neighbours++;
+////       }
 //      }
-     }
-    }
+//     }
+//    }
 //    if (board[i][j]) {
 //     neighbours -= 1;
 //    }
-    if (neighbours <= 2 || neighbours > 3) {
-     future[i][j] = false;
-    } else {
-     future[i][j] = true;
-    }
-   }
+//    changes.put(new SimpleEntry<>(i, j), rules.step(i, j, neighbours, board[i][j]));
+//
+////////    if (neighbours <= 2 || neighbours > 3) {
+//////    if (neighbours < 2 || neighbours > 3) {
+////////     future[i][j] = false;
+//////     changes.put(new SimpleEntry<>(i, j), true);
+//////    } else {
+////////     future[i][j] = true;
+//////     changes.put(new SimpleEntry<>(i, j), false);
+//////    }
+////
+//////    if (neighbours == 2 || neighbours == 3) {
+//////     future[i][j] = true;
+//////    } else {
+//////     future[i][j] = false;
+//////    }
+////
+////    if (neighbours == 2) {
+////     changes.put(new SimpleEntry<>(i, j), board[i][j]);
+//////     future[i][j] = board[i][j];
+////    } else if (neighbours == 3) {
+////     changes.put(new SimpleEntry<>(i, j), true);
+//////     future[i][j] = true;
+////    } else {
+////     changes.put(new SimpleEntry<>(i, j), false);
+//////     future[i][j] = false;
+////    }
+//   }
+//  }
+//  for (final SimpleEntry<Integer, Integer> change : changes.keySet()) {
+//   board[change.getKey()][change.getValue()] = changes.get(change);
+//  }
+//  changes.clear();
+  for (final StepFunction sf : sfs) {
+   sf.step(this);
   }
-  return board = future;
+  return board;
  }
 
  public Boolean[][] peek() {
-  return Arrays.copyOf(board, board.length);
+//  return Arrays.copyOf(board, board.length);
+  return board;
  }
 
  public void set(int i, int j, boolean value) {
--- src/java/cgol/src/io/github/matthewacon/cgol/Main.java
+++ src/java/cgol/src/io/github/matthewacon/cgol/Main.java
@@ -2,14 +2,76 @@ package io.github.matthewacon.cgol;
 
 import processing.core.PApplet;
 
+import java.util.AbstractMap.SimpleEntry;
+import java.util.LinkedHashMap;
+
 public class Main extends PApplet {
  public static void main(String[] args) {
   PApplet.main(Main.class.getCanonicalName());
  }
 
+ public static final CGOL.StepFunction
+  CONWAY = cgol -> {
+   final Boolean[][] board = cgol.peek();
+   final LinkedHashMap<SimpleEntry<Integer, Integer>, Boolean> changes = new LinkedHashMap<>();
+   for (int i = 0; i < board.length; i++) {
+    for (int j = 0; j < board[i].length; j++) {
+     //Count neighbours
+     int neighbours = 0;
+     for (int l = -1; l < 2; l++) {
+      for (int m = -1; m < 2; m++) {
+       final SimpleEntry<Integer, Integer> indices = cgol.wrap(i + l, j + m);
+       if (board[indices.getKey()][indices.getValue()]) {
+        neighbours++;
+       }
+      }
+     }
+     if (board[i][j]) {
+      neighbours -= 1;
+     }
+     if (neighbours == 2) {
+      changes.put(new SimpleEntry<>(i, j), board[i][j]);
+     } else if (neighbours == 3) {
+      changes.put(new SimpleEntry<>(i, j), true);
+     } else {
+      changes.put(new SimpleEntry<>(i, j), false);
+     }
+    }
+   }
+   for (final SimpleEntry<Integer, Integer> change : changes.keySet()) {
+    board[change.getKey()][change.getValue()] = changes.get(change);
+   }
+  },
+  ANTI_CONWAY = cgol -> {
+   final Boolean[][] board = cgol.peek();
+   for (int i = 0; i < board.length; i++) {
+    for (int j = 0; j < board[i].length; j++) {
+     //Count neighbours
+     int neighbours = 0;
+     for (int l = -1; l < 2; l++) {
+      for (int m = -1; m < 2; m++) {
+      if (!(l == i && m == j)) {
+       final SimpleEntry<Integer, Integer> indices = cgol.wrap(i + l, j + m);
+       if (board[indices.getKey()][indices.getValue()]) {
+        neighbours++;
+       }
+       }
+      }
+     }
+//    if (neighbours <= 2 || neighbours > 3) {
+//    if (neighbours < 2 || neighbours > 3) {
+     if (neighbours == 2 || neighbours == 3) {
+      board[i][j] = true;
+     } else {
+      board[i][j] = false;
+     }
+    }
+   }
+  };
+
  private int
-  boardWidth = 50,
-  boardHeight = 50,
+  boardWidth = 100,
+  boardHeight = 100,
   cellWidth,
   cellHeight;
 
@@ -17,37 +79,44 @@ public class Main extends PApplet {
 
  private boolean
   mouseHeld = false,
-  shouldUpdate = false;
+  shouldUpdate = false,
+  enableGrid = false,
+  strangeRules = false;
 
  private long
   lastStep = 0,
-  updateDelay = 800;
+  updateDelay = 100;
 
  public void settings() {
-  size(800, 800, "processing.awt.PGraphicsJava2D");
+  size(1000, 1000, "processing.awt.PGraphicsJava2D");
  }
 
  public void setup() {
   cellWidth = Math.floorDiv(width, boardWidth);
   cellHeight = Math.floorDiv(height, boardHeight);
-//  cgol.generateRandomBoard();
  }
 
  public void draw() {
   background(127, 127, 127);
   fill(0);
   strokeWeight(0);
-//  stroke(63, 127, 255);
-  stroke(0);
+  if (enableGrid) {
+   stroke(63, 127, 255);
+  } else {
+   stroke(0);
+  }
+//  stroke(0);
   //Mouse input
   if (mouseHeld) {
-   final int
-    x = Math.floorDiv(mouseX, cellWidth),
-    y = Math.floorDiv(mouseY, cellHeight);
-   if (mouseButton == LEFT) {
-    cgol.set(x, y, true);
-   } else if (mouseButton == RIGHT) {
-    cgol.set(x, y, false);
+   if (!(mouseX > width - 1 || 0 > mouseX) && !(mouseY > height - 1 || 0 > mouseY)) {
+    final int
+     x = Math.floorDiv(mouseX, cellWidth),
+     y = Math.floorDiv(mouseY, cellHeight);
+    if (mouseButton == LEFT) {
+     cgol.set(x, y, true);
+    } else if (mouseButton == RIGHT) {
+     cgol.set(x, y, false);
+    }
    }
   }
   //Draw cells
@@ -63,9 +132,8 @@ public class Main extends PApplet {
     popMatrix();
    }
   }
-  //Staggered grid updates
-  if (shouldUpdate && (System.currentTimeMillis() - lastStep) / (updateDelay + 0d) >= 1d) {
-   cgol.step();
+  if (shouldUpdate && (System.currentTimeMillis() - lastStep) > updateDelay) {
+   cgol.step(strangeRules ? ANTI_CONWAY : CONWAY);
    lastStep = System.currentTimeMillis();
   }
  }
@@ -81,9 +149,13 @@ public class Main extends PApplet {
    } else if (key == 'c') {
     cgol.clear();
    } else if (key == '-') {
-    updateDelay += 50;
+    updateDelay += 25;
    } else if (key == '=') {
-    updateDelay = updateDelay - 50 > 0 ? updateDelay - 50 : updateDelay;
+    updateDelay = updateDelay - 25 > 0 ? updateDelay - 25 : updateDelay;
+   } else if (key == 'g') {
+    enableGrid = !enableGrid;
+   } else if (key == 'm') {
+    strangeRules = !strangeRules;
    }
   }
  }
